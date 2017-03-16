5ff30395f26243497cc636d2686af60316c8cff4
hbran <12bran6009@gmail.com>
2017-03-16 11:15:14 +0900

    add Loop.Java

--- JavaPractice/src/org/dimigo/basic/Loop.java
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -4,6 +4,7 @@
 package org.dimigo.basic;
 
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * <pre>
@@ -19,69 +20,35 @@ import java.util.Scanner;
  */
 public class Loop {
     public static void main(String[] args) {
-
-        /*int [] arr = {1,2,3,4,5};
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
-        */
-        String[] twice = {"나연","사나","쯔위","정연"};
-
-        for (String m : twice ){
-            System.out.println(m);
-        }
-
         Scanner scanner = new Scanner(System.in);
-        int choice;
-
+        int a;
+        String[] arr = {"마법사","영주","기사","농민"};
+        int hp = 100;
         do {
-            System.out.println("<<메뉴 선택>>");
-            System.out.println("1. 캐비어");
-            System.out.println("2. 샥스핀");
+            System.out.println("<<게임 메뉴>>");
+            System.out.println("1. 공격력 증가");
+            System.out.println("2. 공격력 감소");
+            System.out.println("3. 캐릭터 설정");
             System.out.println("9. 종료");
-            System.out.println("==== 당신의 선택은 ?");
-
-            choice=scanner.nextInt();
-            switch (choice) {
+            a = scanner.nextInt();
+            switch (a) {
                 case 1:
-                    System.out.println("호우");
+                    hp += 10;
+                    System.out.println("공격력이 증가되었습니다. 현재 공격력" + hp);
                     break;
                 case 2:
-                    System.out.println("호우");
+                    hp-=10;
+                    System.out.println("공격력이 감소되었습니다. 현재 공격력" + hp);
+                    break;
+                case 3:
+                    int num = new Random().nextInt(4);
+                    System.out.println(arr[num]+"(으)로 설정되었습니다");
                     break;
                 case 9:
-                    System.out.println("호우");
+                    System.out.println("이제 공부하세요!");
                     break;
-                default:
-                    System.out.println("없는데??");
-                    break;
+
             }
-        }while (true);
-
-
-        /*int a;
-        int hp = 100;
-        System.out.println("<<게임 메뉴>>");
-        System.out.println("1. 공격력 증가");
-        System.out.println("2. 공격력 감소");
-        System.out.println("3. 캐릭터 설정");
-        System.out.println("9. 종료");
-
-        switch (a) {
-            case 1:
-                System.out.println("공격력이 증가되었습니다. 현재 공격력"+hp+10);
-                break;
-            case 2:
-                System.out.println("공격력이 감소되었습니다. 현재 공격력"+(hp-10));
-                break;
-            case 3:
-                System.out.println("공격력이 증가되었습니다. 현재 공격력"+hp+10);
-                break;
-            case 9:
-                System.out.println("공격력이 증가되었습니다. 현재 공격력"+hp+10);
-
-        }
-    }
-    */
+        } while(a!=9);
     }
 }
